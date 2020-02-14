2d07f39fb33951947b177a1513c1ae0dd79dfd8d
ekiprop <evanskiprop1@gmail.com>
2020-02-14 09:34:22 +0300

    nexus prisma genarate

--- index.ts
+++ index.ts
@@ -65,3 +65,9 @@ const schema = makePrismaSchema({
     typegen: path.join(__dirname, "./generated/nexus.ts")
   }
 });
+
+const server = new GraphQLServer({
+  schema,
+  context: { prisma }
+});
+server.start(() => console.log("Server is running on http://localhost:4000"));
